edaf13e410f6be3d3ec7d89044f95f441aa595da
xxninjabunnyxx <xxnninjabunnyxx@gmail.com>
2020-12-13 20:41:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const npm = require('npm')
+
+let pjson = JSON.parse(fs.readFileSync('./package.json'))
+
+pjson.scripts = {
+    setup: 'npm i -D @types/node typescript concurrently'
+}
+fs.writeFileSync('./package.json', JSON.stringify(pjson, undefined, 2))
+
+npm.load(() => {
+    npm.run('setup', () => {
+        let pjson = JSON.parse(fs.readFileSync('./package.json'))
+        delete pjson.scripts.setup
+        pjson.scripts.build = "npx tsc"
+        pjson.scripts.start =  "npm run build && node dist/server.js",
+        pjson.scripts.dev =  "concurrently \"npm run build -- -w\" \"npx nodemon dist/server.js\"",
+        fs.writeFileSync('./package.json', JSON.stringify(pjson, undefined, 2))
+    })
+})
+
